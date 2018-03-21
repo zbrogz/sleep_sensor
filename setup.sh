1df184144cbfaade7205389eb1b15911ee6b2b7a
Zach Brogan <zachbrogan@gmail.com>
2018-03-21 09:53:37 -0600

    Script uzip fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip2 install adafruit-io
-unzip ModuleConnector/ModuleConnector-rpi-1.4.3.zip
+unzip ModuleConnector/ModuleConnector-rpi-1.4.3.zip -d ModuleConnector/
 python2 ModuleConnector/python27-arm-linux-gnueabihf/setup.py install
 sed -i '/exit 0/i \
 sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &' /etc/rc.local
