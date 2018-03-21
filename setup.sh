27b71bdb0e282648be8e75fe540b06fa13c5f07b
Zach Brogan <zachbrogan@gmail.com>
2018-03-21 09:30:25 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+pip2 install adafruit-io
+unzip ModuleConnector-rpi-1.4.3.zip -d ../ModuleConnector
+python2 ../ModuleConnector/python27-arm-linux-gnueabihf/setup.py
+sed -i '/exit 0/i \
+sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &' /etc/rc.local
