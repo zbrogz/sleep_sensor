2cb69a26321c55c08524d4040037e875d70cef8d
Zach Brogan <zachbrogan@gmail.com>
2018-03-22 08:44:04 -0600

    Echo

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ cd python27-arm-linux-gnueabihf
 python2 setup.py install
 sed -i '/exit 0/i \
 sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &' /etc/rc.local
+echo Sleep Sensor Install Complete
