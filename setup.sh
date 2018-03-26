b53a93b3e214d0d180f0a4e2a79d25a4e1a9bf0f
Zach Brogan <zachbrogan@gmail.com>
2018-03-26 09:19:54 -0600

    Modifying startup script (rc.local) checks if command already written

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ pip2 install adafruit-io
 unzip ModuleConnector-rpi-1.4.3.zip
 cd python27-arm-linux-gnueabihf
 python2 setup.py install
-sed -i '/exit 0/i \
-sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &' /etc/rc.local
+grep -q -F 'sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &' /etc/rc.local || sudo sed -i "`wc -l < /etc/rc.local`i\\sudo python2 /home/pi/sleep_sensor/sleep_algorithm.py &\\" /etc/rc.local
 echo Sleep Sensor Install Complete
